cc2bb7cbbc554b1ca90ce51a30b6ae39c193081c
tawashley <trisashley@gmail.com>
2016-05-23 11:24:23 +0100

    chore: Remove responsiveImages object key from config file

--- _config/task.images.js
+++ _config/task.images.js
@@ -11,27 +11,6 @@ function getTaskConfig(projectConfig) {
 		svgs: [
 			projectConfig.paths.src.images + '**/*.svg'
 		],
-		responsiveImages: {
-			src: [
-				projectConfig.paths.src.images + 'responsive/**/*.{png,jpg}',
-			],
-			config: {
-				//
-				//https://github.com/mahnunchik/gulp-responsive
-				//
-				// "path/to/images": [
-				// 	{
-				// 		"width":   1020,
-				// 		"height":  1020,
-				// 		"quality": 60,
-				// 		"rename": {
-				// 			"suffix":  "-large"
-				// 		},
-				// 		"crop":    "center"
-				// 	},
-				// ]
-			}
-		},
 		watch: [
 			projectConfig.paths.src.images + '**/*.{png,jpg}',
 			projectConfig.paths.src.images + '**/*.svg'
